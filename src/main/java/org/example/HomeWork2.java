60255a1193bcd470ec07927997c92d969bae2502
Elza35 <Elzamuksinova@mail.ru>
2025-10-30 12:31:19 +0300

    Lesson 2: add methods 1-3 (printThreeWords, checkSumSign, printColor)

---
+++ src/main/java/org/example/HomeWork2.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class HomeWork2 {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -3;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 75;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Домашняя работа 2 начата!");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+    }
+}
