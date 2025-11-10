044a3685ec73fc2e13c907497769f55b0f64f6b0
Elza35 <Elzamuksinova@mail.ru>
2025-11-10 14:29:08 +0300

    5: обавление MyArrayDataException и метода суммирования

--- src/main/java/Lesson5/ArrayProcessor.java
+++ src/main/java/Lesson5/ArrayProcessor.java
@@ -1,47 +1,38 @@
 public class ArrayProcessor {
+
     public static void validateArraySize(String[][] array) throws MyArraySizeException {
-        if (array.length !=4) {
-            throw new MyArraySizeException("Ошибка: Массив должен иметь 4 строки, но получено: " + array.lenght);
-                }
-        For (int i = 0; i < array.length; i++) {
-            if (array) [i].length != 4) {
-    throw new MyArraySizeException(
-            String.format("Ошибка: строка %d должна иметь 4 столбца, но имеет: %d", i, array[i].length)
-    );
+        if (array.length != 4) {
+            throw new MyArraySizeException(
+                    "Ошибка: массив должен иметь 4 строки, но получено: " + array.length
+            );
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException(
+                        String.format("Ошибка: строка %d должна иметь 4 столбца, но имеет: %d", i, array[i].length)
+                );
             }
         }
-        System.out.println("✓ Массив корректного размера 4x4");
     }
 
-    public static void main(String[] args) {
-        // Тестовые данные
-        String[][] correctArray = {
-                {"1", "2", "3", "4"},
-                {"5", "6", "7", "8"},
-                {"9", "10", "11", "12"},
-                {"13", "14", "15", "16"}
-        };
+    /**
+     * Метод для суммирования всех элементов массива
+     */
+    public static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        // Сначала проверяем размер
+        validateArraySize(array);
 
-        String[][] wrongSizeArray = {
-                {"1", "2", "3"},
-                {"4", "5", "6"},
-                {"7", "8", "9"}
-        };
-
-        try {
-            System.out.println("Тест 1: Корректный массив");
-            validateArraySize(correctArray);
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j, array[i][j]);
+                }
+            }
         }
-
-        try {
-            System.out.println("\nТест 2: Массив неверного размера");
-            validateArraySize(wrongSizeArray);
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        }
+        return sum;
     }
-}
-
 }
